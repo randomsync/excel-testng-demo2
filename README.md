8d29e27da35d6247e2f3d9407387f5c757524b3a
Gaurav Gupta <gaurav@randomsync.net>
2013-12-10 22:37:27 -0800

    Partial commit (WIP)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 excel-testng-demo2
 ==================
 
-Demo project for excel-testng
+Project to test dependencies in TestNG and to test ordering of test methods
--- src/main/java/net/randomsync/testng/excel/demo/DependencyTest.java
+++ src/main/java/net/randomsync/testng/excel/demo/DependencyTest.java
@@ -1,15 +1,44 @@
 package net.randomsync.testng.excel.demo;
 
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class DependencyTest {
 
 	@Parameters({ "message" })
-	@Test
-	public void test1(String message) {
-		System.out.println("message=" + message);
-
+	@Test(groups = { "b" })
+	public void test1(@Optional("default message") String message) {
+		System.out.println("Test1. Message=" + message);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	@Parameters({ "message" })
+	@Test(groups = { "a" })
+	public void test2(@Optional("default message") String message) {
+		System.out.println("Test2. Message=" + message);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Parameters({ "message" })
+	@Test(groups = { "init" })
+	public void test3(@Optional("default message") String message) {
+		System.out.println("Test3. Message=" + message);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
